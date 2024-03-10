4ef047d38145a3b38affc18d7d1f49dcac9f3b73
Mikhail Ilinsky <mail@milinsky.ru>
2024-03-10 20:12:25 +1000

    Add selective reset by Reset attribute

---
+++ tests/Functional/ResetTest.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Duyler\DependencyInjection\Test\Functional;
+
+use Duyler\DependencyInjection\Attribute\Reset;
+use Duyler\DependencyInjection\Container;
+use Duyler\DependencyInjection\Exception\ResetNotImplementException;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+class ResetTest extends TestCase
+{
+    #[Test]
+    public function with__implement_reset(): void
+    {
+        $container = new Container();
+        $obj = $container->get(WithReset::class);
+        $obj->setName('test');
+        $this->assertEquals('test', $obj->getName());
+        $obj->setName('test2');
+        $this->assertEquals('test2', $obj->getName());
+        $container->selectiveReset();
+        $this->assertNull($obj->getName());
+    }
+
+    #[Test]
+    public function without__implement_reset(): void
+    {
+        $container = new Container();
+        $container->get(WithoutReset::class);
+
+        $this->expectException(ResetNotImplementException::class);
+        $container->selectiveReset();
+    }
+}
+
+#[Reset]
+class WithReset
+{
+    private ?string $name = null;
+
+    public function reset(): void
+    {
+        $this->name = null;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+}
+
+#[Reset]
+class WithoutReset {}
